063732cba6e176c6e50ebcb2a9a11f08b6bf311c
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-08-01 13:32:38 -0700

    Refactor modal components to improve structure and readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { IoClose } from "react-icons/io5";
 import LoginModal from "./components/LoginModal";
+import SignupModal from "./components/SignupModal";
+import ForgotPasswordModal from "./components/ForgotPasswordModal";
 
 const App = () => {
   const [isLoginModal, setIsLoginModal] = useState(false);
@@ -39,91 +40,26 @@ const App = () => {
           </button>
         </div>
       </div>
+      
       {isLoginModal && (
-        <div className="flex flex-col justify-center items-center min-h-screen w-screen bg-black/50 fixed top-0 left-0">
-          <div className="w-[400px] bg-white p-6 shadow rounded relative">
-            <div className="flex justify-between">
-              <div className="mb-4">
-                <h1 className="text-xl font-semibold">Login Information</h1>
-                <IoClose size={20} onClick={() => setIsLoginModal(false)} className="cursor-pointer absolute top-5 right-5" />
-              </div>
-            </div>
-            <div className="flex flex-col gap-4">
-              <input type="text" placeholder="Username" className="w-full rounded bg-gray-200 px-4 py-2" />
-              <input type="password" placeholder="Password" className="w-full rounded bg-gray-200 px-4 py-2" />
-              <p className="text-blue-500 hover:underline cursor-pointer">
-                <button 
-                  className="cursor-pointer" 
-                  onClick={() => {
-                    setIsForgotPasswordModal(true),
-                    setIsLoginModal(false);
-                  }}>
-                  Forgot Password?
-                </button>
-              </p>
-              <button className="w-full bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Sign In</button>
-              <p className="text-center text-sm text-gray-600">
-                Don't have an Account?{" "}
-                <button
-                  className="text-blue-500 hover:underline cursor-pointer"
-                  onClick={() => {
-                    setIsSignUpModal(true);
-                    setIsLoginModal(false);
-                  }}
-                >
-                  Sign Up
-                </button>
-              </p>
-            </div>
-          </div>
-        </div>
+        <LoginModal 
+          setIsLoginModal={setIsLoginModal}
+          setIsSignUpModal={setIsSignUpModal}
+          setIsForgotPasswordModal={setIsForgotPasswordModal}
+        />
       )}
+      
       {isSignUpModal && (
-        <div className="flex flex-col justify-center items-center min-h-screen w-screen bg-black/50 fixed top-0 left-0">
-          <div className="w-[400px] bg-white p-6 shadow rounded relative">
-            <div className="flex justify-between">
-              <div className="mb-4">
-                <h1 className="text-xl font-semibold">Sign Up</h1>
-                <IoClose size={20} onClick={() => setIsSignUpModal(false)} className="cursor-pointer absolute top-5 right-5" />
-              </div>
-            </div>
-            <div className="flex flex-col gap-4">
-              <input type="text" placeholder="Username" className="w-full rounded bg-gray-200 px-4 py-2" />
-              <input type="email" placeholder="Email" className="w-full rounded bg-gray-200 px-4 py-2" />
-              <input type="password" placeholder="Password" className="w-full rounded bg-gray-200 px-4 py-2" />
-              <button className="w-full bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">Sign Up</button>
-              <p className="text-center text-sm text-gray-600">
-                Already have and Account?{" "}
-                <button
-                  className="text-blue-500 hover:underline cursor-pointer"
-                  onClick={() => {
-                    setIsLoginModal(true);
-                    setIsSignUpModal(false);
-                  }}
-                >
-                  Sign In
-                </button>
-              </p>
-            </div>
-          </div>
-        </div>
+        <SignupModal 
+          setIsSignUpModal={setIsSignUpModal}
+          setIsLoginModal={setIsLoginModal}
+        />
       )}
+      
       {isForgotPasswordModal && (
-        <div className="flex flex-col justify-center items-center min-h-screen w-screen bg-black/50 fixed top-0 left-0">
-          <div className="w-[400px] bg-white p-6 shadow rounded relative">
-            <div className="flex justify-between">
-              <div className="mb-4">
-                <h1 className="textxl font-bold">Reset Password</h1>
-                <IoClose size={28} onClick={() => setIsForgotPasswordModal(false)} className="cursor-pointer absolute top-5 right-5" />
-              </div>
-            </div>
-            <div className="flex flex-col gap-4">
-              <p className="font-medium">Enter your email address Address, the we will send you OTP</p>
-              <input type="email" placeholder="Email" className="w-full rounded bg-gray-200 px-4 py-2" />
-              <button className="w-full bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Reset Password</button>
-            </div>
-          </div>
-        </div>
+        <ForgotPasswordModal 
+          setIsForgotPasswordModal={setIsForgotPasswordModal}
+        />
       )}
     </div>
   );
