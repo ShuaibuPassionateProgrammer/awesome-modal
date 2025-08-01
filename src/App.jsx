36aac7ac8d9c910f7db36ed15deab8e340fa0b18
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-08-01 13:11:22 -0700

    Update "Forgot Password" modal to improve clarity and user instructions

--- src/App.jsx
+++ src/App.jsx
@@ -112,13 +112,13 @@ const App = () => {
           <div className="w-[400px] bg-white p-6 shadow rounded relative">
             <div className="flex justify-between">
               <div className="mb-4">
-                <h1 className="textxl fotn-semibold">Forgot Password</h1>
+                <h1 className="textxl font-bold">Reset Password</h1>
                 <IoClose size={28} onClick={() => setIsForgotPasswordModal(false)} className="cursor-pointer absolute top-5 right-5" />
               </div>
             </div>
             <div className="flex flex-col gap-4">
-              <input type="email" className="w-full rounded bg-gray-200 px-4 py-2" />
-              <input type="password" placeholder="Password" className="w-full rounded bg-gray-200 px-4 py-2" />
+              <p className="font-medium">Enter your email address Address, the we will send you OTP</p>
+              <input type="email" placeholder="Email" className="w-full rounded bg-gray-200 px-4 py-2" />
               <button className="w-full bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Reset Password</button>
 
             </div>
